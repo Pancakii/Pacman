435a557e922a4b711124ce4270b9718667285689
EmirhanFaruk <emirhansagir74@gmail.com>
2023-11-18 17:21:33 +0100

    update from develop

--- src/main/java/pathfinding/Pathfinding.java
+++ src/main/java/pathfinding/Pathfinding.java
@@ -1,19 +1,20 @@
-package gui;
+package pathfinding;
 
-import config.Cell;
 import geometry.RealCoordinates;
 
-public class Node
+public class Pathfinding
 {
-    RealCoordinates coordinates;
+    static RealCoordinates start;
+    static RealCoordinates end;
+
     int used;
     int to_use;
     int total;
-    Node[] neighbors = new Node[4]; // 0: nord, 1: est, 2: sud, 3: ouest
+    Pathfinding[] neighbors = new Pathfinding[4]; // 0: nord, 1: est, 2: sud, 3: ouest
     boolean open;
     
     /*
-    public Node(int i, int j, int used, int di, int dj)
+    public Pathfinding(int i, int j, int used, int di, int dj)
     {
         coordinates = new RealCoordinates(i, j);
         this.used = used;
@@ -30,36 +31,36 @@ public class Node
         if(coordinates.round().x() >= 1) {
             if (!tab[coordinates.round().x() - 1][coordinates.round().y()].isWall()) //si neighbor n'est pas un mur
             {
-                neighbors[0] = new Node(coordinates.x() - 1, coordinates.y(), used + 1, di, dj);
+                neighbors[0] = new Pathfinding(coordinates.x() - 1, coordinates.y(), used + 1, di, dj);
             }
         }
         //EST
         if(coordinates.round().y() <= tab[0].length-1) {
             if (!tab[coordinates.round().x()][coordinates.round().y() + 1].isWall()) //si neighbor n'est pas un mur
             {
-                neighbors[0] = new Node(coordinates.x, coordinates.y + 1, used + 1, di, dj);
+                neighbors[0] = new Pathfinding(coordinates.x, coordinates.y + 1, used + 1, di, dj);
             }
         }
         //SUD
         if(coordinates.round().x() <= tab.length-1) {
             if (!tab[coordinates.round().x() + 1][coordinates.round().y()].isWall()) //si neighbor n'est pas un mur
             {
-                neighbors[0] = new Node(coordinates.x + 1, coordinates.y, used + 1, di, dj);
+                neighbors[0] = new Pathfinding(coordinates.x + 1, coordinates.y, used + 1, di, dj);
             }
         }
         //OUEST
         if(coordinates.round().y() >= 1) {
             if (!tab[coordinates.round().x()][coordinates.round().y() - 1].isWall()) //si neighbor n'est pas un mur
             {
-                neighbors[0] = new Node(coordinates.x, coordinates.y - 1, used + 1, di, dj);
+                neighbors[0] = new Pathfinding(coordinates.x, coordinates.y - 1, used + 1, di, dj);
             }
         }
     }*/
 
-    int calculateDistance(int di, int dj)
+    static int calculateDistance(int di, int dj)
     {
-        int x = Math.abs(di-coordinates.round().x());
-        int y = Math.abs(dj-coordinates.round().y());
+        int x = Math.abs(di-end.round().x());
+        int y = Math.abs(dj-end.round().y());
         return x + y;
     }
 
