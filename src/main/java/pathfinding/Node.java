40e2f8ad913500c32005712e5c39bdad29384a1a
EmirhanFaruk <emirhansagir74@gmail.com>
2023-11-18 22:35:52 +0100

    Finished working on Node.java, will implement it to ghosts in the next commits.

--- src/main/java/pathfinding/Node.java
+++ src/main/java/pathfinding/Node.java
@@ -1,21 +1,21 @@
 package pathfinding;
 
+import config.Cell;
 import geometry.RealCoordinates;
-import model.Critter;
 
 import java.util.ArrayList;
 
 public class Node
 {
     private Node parent;
-    private RealCoordinates coordinates;
+    private final RealCoordinates coordinates;
 
-    private final Node[] neighbors = new Node[4]; // 0: nord, 1: est, 2: sud, 3: ouest
+    private ArrayList<Node> neighbors = new ArrayList<>(); // 0: nord, 1: est, 2: sud, 3: ouest
 
-    private double f = Double.MAX_VALUE;
-    private double g = Double.MAX_VALUE;
+    private double f = Double.MAX_VALUE; // total of move and toGo
+    private double g = Double.MAX_VALUE; // the cost already used
 
-    private double h;
+    private double h; // the cost to go
 
 
 
@@ -31,88 +31,202 @@ public class Node
         return x + y;
     }
 
-
-
-
-
-    void calculateTotal()
+    void calculateTotal(RealCoordinates end)
     {
         // F(Total cost function), total of move and toGo
+        toGo(end);
         f = g + h;
     }
 
-    void move()
-    {
-        // G(move) function, the cost already used
-
-    }
-
-    void toGo()
+    void toGo(RealCoordinates end)
     {
         // Heuristic function, the cost to go
-
+        h = calculateDistance(coordinates, end);
     }
 
-    /*
+
     private static boolean same(Node a, Node b)
     {
         // Verifie si les deux noeuds ont la meme coordonnées
         return a.coordinates.x() == b.coordinates.x() && a.coordinates.y() == b.coordinates.y();
     }
 
-    private ArrayList<Node> getNeighbors(Cell[][] tab, int di, int dj)
+    private void getNeighbors(Cell[][] tab)
     {
+        neighbors = new ArrayList<>();
+        // The function that adds neighbors.
         //NORD
         if(coordinates.round().x() >= 1) {
             if (!tab[coordinates.round().x() - 1][coordinates.round().y()].isWall()) //si neighbor n'est pas un mur
             {
-                neighbors[0] = new Pathfinding(coordinates.x() - 1, coordinates.y(), used + 1, di, dj);
+                Node node = new Node(new RealCoordinates(coordinates.x() - 1, coordinates.y()));
+                node.g = this.g + 1;
+                node.parent = this;
+                neighbors.add(node);
             }
         }
         //EST
         if(coordinates.round().y() <= tab[0].length-1) {
             if (!tab[coordinates.round().x()][coordinates.round().y() + 1].isWall()) //si neighbor n'est pas un mur
             {
-                neighbors[0] = new Pathfinding(coordinates.x, coordinates.y + 1, used + 1, di, dj);
+                Node node = new Node(new RealCoordinates(coordinates.x(), coordinates.y() + 1));
+                node.g = this.g + 1;
+                node.parent = this;
+                neighbors.add(node);
             }
         }
         //SUD
         if(coordinates.round().x() <= tab.length-1) {
             if (!tab[coordinates.round().x() + 1][coordinates.round().y()].isWall()) //si neighbor n'est pas un mur
             {
-                neighbors[0] = new Pathfinding(coordinates.x + 1, coordinates.y, used + 1, di, dj);
+                Node node = new Node(new RealCoordinates(coordinates.x() + 1, coordinates.y()));
+                node.g = this.g + 1;
+                node.parent = this;
+                neighbors.add(node);
             }
         }
         //OUEST
         if(coordinates.round().y() >= 1) {
             if (!tab[coordinates.round().x()][coordinates.round().y() - 1].isWall()) //si neighbor n'est pas un mur
             {
-                neighbors[0] = new Pathfinding(coordinates.x, coordinates.y - 1, used + 1, di, dj);
+                Node node = new Node(new RealCoordinates(coordinates.x(), coordinates.y() - 1));
+                node.g = this.g + 1;
+                node.parent = this;
+                neighbors.add(node);
             }
         }
     }
 
-    public static Node aStar(RealCoordinates s, RealCoordinates e)
+    public static Node findLowestCost(ArrayList<Node> list)
     {
+        // Self-explanatory name
+        // We are sure that the list is not empty
+        Node res = list.get(0);
+        for (Node n : list)
+        {
+            if(res.g > n.g)
+            {
+                res = n;
+            }
+        }
+        return res;
+    }
+
+    public static void addListDistinctive(ArrayList<Node> list, Node to_add)
+    {
+        // Adds element to the list but swaps if coordinates are the same
+        Node n = sameIn(list, to_add);
+        if(n != null)
+        {
+            if (n.f > to_add.f)
+            {
+                list.remove(n);
+                list.add(to_add);
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+        list.add(to_add);
+    }
+
+    public static Node sameIn(ArrayList<Node> list, Node element)
+    {
+        // Returns it if a node with same coordinates exist. If not, returns null.
+        for (Node n : list)
+        {
+            if(Node.same(n, element))
+            {
+                return n;
+            }
+        }
+        return null;
+    }
+
+    public static boolean exists(ArrayList<Node> list, Node element)
+    {
+        for (Node n : list)
+        {
+            if(Node.same(n, element))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    public static Node aStar(RealCoordinates s, RealCoordinates e, Cell[][] grid)
+    {
+        // Setting the variables that will be used later
         Node start = new Node(s);
         Node end = new Node(e);
         ArrayList<Node> closedList = new ArrayList<>();
         ArrayList<Node> openList = new ArrayList<>();
 
-        start.calculateTotal();
+        // Initializing for the loop
+        start.calculateTotal(e);
         openList.add(start);
+
+
         while(!openList.isEmpty())
         {
-            Node n = openList.get(-1);
-            if(Node.same(n, end))
+            Node current = Node.findLowestCost(openList);
+            if(Node.same(current, end))
             {
-                return n;
+                return current;
             }
 
+            current.getNeighbors(grid);
+            current.calculateTotal(e);
+            addListDistinctive(closedList, current);
 
+            for(Node neighbor : current.neighbors)
+            {
+                boolean flag = false;
+                for(Node c : closedList)
+                {
+                    if(Node.same(neighbor, c))
+                    {
+                        flag = true;
+                    }
+                }
+
+                if(!flag)
+                {
+                    neighbor.getNeighbors(grid);
+                    // if in open and new path shorter
+                    Node checker = Node.sameIn(openList, neighbor);
+                    if((checker != null && neighbor.f < checker.f))
+                    {
+                        checker.f = neighbor.f;
+                        checker.parent = current;
+                    }
+                    if(!exists(openList, neighbor))
+                    {
+                        neighbor.calculateTotal(e);
+                        openList.add(neighbor);
+                    }
+                }
+            }
         }
 
         return null;
     }
-    */
+
+
+    public static ArrayList<RealCoordinates> getPath(RealCoordinates s, RealCoordinates e, Cell[][] grid)
+    {
+        ArrayList<RealCoordinates> res = new ArrayList<>();
+        Node node = aStar(s, e, grid);
+        while(node != null)
+        {
+            res.add(0, node.coordinates);
+            node = node.parent;
+        }
+        return res;
+    }
 }
