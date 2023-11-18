1a5366f2dd365a5bf25b38e1e8558b0ac3707ea4
EmirhanFaruk <emirhansagir74@gmail.com>
2023-11-18 19:39:01 +0100

    Updates on Node.java and maybe the start of a solution to the restart game thing

--- src/main/java/gui/Jeu.java
+++ src/main/java/gui/Jeu.java
@@ -14,10 +14,11 @@ public class Jeu  implements EventHandler<ActionEvent> {
         Stage primaryStage = new Stage();
         var root = new Pane();
         var gameScene = new Scene(root);
-        var pacmanController = new PacmanController(MazeConfig.make());
+        MazeConfig maze_config = MazeConfig.make();
+        var pacmanController = new PacmanController(maze_config);
         gameScene.setOnKeyPressed(pacmanController::keyPressedHandler);
         gameScene.setOnKeyReleased(pacmanController::keyReleasedHandler);
-        var maze = new MazeState(MazeConfig.make());
+        var maze = new MazeState(maze_config);
         var gameView = new GameView(maze, root, 35.0);
         primaryStage.setScene(gameScene);
         primaryStage.show();
