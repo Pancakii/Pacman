974ca87e008af1a767dfd687dba092fab613675e
EmirhanFaruk <emirhansagir74@gmail.com>
2023-11-30 23:14:41 +0100

    Now the ghosts move but towards the wall, they can get out of the base.

--- src/main/java/model/Ghost.java
+++ src/main/java/model/Ghost.java
@@ -4,7 +4,6 @@ import config.Cell;
 import config.MazeConfig;
 import geometry.IntCoordinates;
 import geometry.RealCoordinates;
-import gui.PacmanController;
 import misc.Debug;
 import pathfinding.Node;
 
@@ -20,12 +19,10 @@ public enum Ghost implements Critter {
     private ArrayList<RealCoordinates> path = new ArrayList<>();
     private RealCoordinates pos;
     private Direction direction = Direction.NONE;
-    private final double direction_timer_max = 10;
-    private double direction_timer = 0;
 
-    private final double path_finding_timer_max = 2;
+    private final double path_finding_timer_max = 0.3;
 
-    private double path_finding_timer = path_finding_timer_max;
+    private double path_finding_timer = 0;
 
     @Override
     public RealCoordinates getPos() {
@@ -57,26 +54,62 @@ public enum Ghost implements Critter {
 
     @Override
     public double getSpeed() {
-        return PacMan.INSTANCE.isEnergized() ? 3.8 : 1.9;
+        return PacMan.INSTANCE.isEnergized() ? 2.9 : 3.8;
+    }
+
+
+
+
+    public void getPathCLYDE(MazeConfig mazeConfig)
+    {
+        // Random
+        RealCoordinates nextPos = pos.plus(DirectionUtils.getVector(direction));
+        IntCoordinates nextCell = nextPos.round();
+
+        RealCoordinates currPost = pos.plus(DirectionUtils.getVector(Direction.NONE));
+        IntCoordinates currCell = currPost.round();
+
+        // Handling cases if there's a wall or if in intersection and if the timer of random direction ended
+        boolean bool = mazeConfig.getCell(nextCell).isWall() || mazeConfig.isIntersection(currCell);
+        int[] base_out_coordinates = {10, 9};
+        if(currCell.x() == base_out_coordinates[0] && currCell.y() == base_out_coordinates[1])
+        {
+            direction = Direction.NORTH;
+        }
+        else if (bool || direction == Direction.NONE)// adding also if direction is none, so it doesn't stop moving
+        {
+            ArrayList<Direction> direcs = new ArrayList<>();
+            for (Direction DIR : new Direction[]{Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST})
+            {
+                nextPos = pos.plus(DirectionUtils.getVector(DIR));
+                nextCell = nextPos.round();
+                if(!mazeConfig.getCell(nextCell).isWall())
+                {
+                    direcs.add(DIR);
+                }
+            }
+            int choice = new Random().nextInt(direcs.size());
+            direction = direcs.get(choice);
+        }
     }
 
     public void getPath(Cell[][] grid, MazeConfig mazeConfig, long delta, RealCoordinates base_coord)
     {
-
         double delta_double = (double) delta;
         path_finding_timer -= delta_double / 1000000000;
-        if(!eaten && path_finding_timer <= 0)
+        if((!eaten && path_finding_timer <= 0) || direction == Direction.NONE)
         {
-            //Debug.out("Start of getPath of " + this);
+            // Debug.out("Start of getPath of " + this);
             // Getting the path to follow.
 
             path_finding_timer = path_finding_timer_max;
 
             RealCoordinates pac_pos = PacMan.INSTANCE.getPos();
+            // Direct chase, goes in front, goes in front, random
             if (this == BLINKY)
             {
-                // Direct chase, goes in front, goes in front, random
                 path = Node.getPath(this.pos, pac_pos, grid);
+                Node.printArrayRC(path);
             }
             else if (this == INKY || this == PINKY)
             {
@@ -107,34 +140,8 @@ public enum Ghost implements Critter {
             }
             else if (this == CLYDE)
             {
-                // Random
-                RealCoordinates nextPos = pos.plus(DirectionUtils.getVector(direction));
-                IntCoordinates nextCell = nextPos.round();
-
-                direction_timer -= delta_double / 1000000000;
-
-                // Handling cases if there's a wall or if the timer of random direction ended
-                boolean bool = (new Random().nextInt() * 10) <= direction_timer || direction_timer <= 0;
-                if (mazeConfig.getCell(nextCell).isWall() || bool)
-                {
-                    ArrayList<Direction> direcs = new ArrayList<>();
-                    for (Direction DIR : new Direction[]{Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST}) {
-                        if (direction != DIR)
-                        {
-                            direcs.add(DIR);
-                        }
-                    }
-                    int choice = new Random().nextInt(direcs.size());
-                    direction = direcs.get(choice);
-                    direction_timer = direction_timer_max;
-                }
+                getPathCLYDE(mazeConfig);
             }
-            else
-            {
-                // If eaten, go to the base
-                path = Node.getPath(this.pos, pac_pos, grid);
-            }
-            //Debug.out("End of getPath of " + this);
         }
         else
         {
@@ -147,7 +154,6 @@ public enum Ghost implements Critter {
             {
                 eaten = false;
             }
-            //Debug.out("End of getPath of " + this);
         }
 
     }
@@ -159,18 +165,32 @@ public enum Ghost implements Critter {
 
         if(!path.isEmpty())
         {
-            //Debug.out("Start of followPath of " + this);
+            Debug.out("Start of followPath of " + this);
             RealCoordinates to_follow = path.get(0);
+            Debug.out(pos.round() + " == " + to_follow.round());
             if (pos.round().same(to_follow.round()))
             {
+                Debug.out("deleting " + path.get(0));
                 path.remove(0);
-                // may add here followPath but im scared of infinite recursion
+                followPath();
+                return;
             }
             else
             {
-                if (to_follow.x() == pos.x())
+                if (to_follow.round().x() == pos.round().x())
                 {
                     if(to_follow.y() > pos.y())
+                    {
+                        direction = Direction.EAST;
+                    }
+                    else
+                    {
+                        direction = Direction.WEST;
+                    }
+                }
+                else if (to_follow.round().y() == pos.round().y())
+                {
+                    if(to_follow.x() > pos.x())
                     {
                         direction = Direction.NORTH;
                     }
@@ -179,31 +199,8 @@ public enum Ghost implements Critter {
                         direction = Direction.SOUTH;
                     }
                 }
-                else if (to_follow.y() == pos.y())
-                {
-                    if(to_follow.x() > pos.x())
-                    {
-                        direction = Direction.WEST;
-                    }
-                    else
-                    {
-                        direction = Direction.EAST;
-                    }
-                }
-                else
-                {
-
-                }
             }
             //Debug.out("End of followPath of " + this);
         }
-        else
-        {
-
-        }
-
     }
-
-
-
 }
