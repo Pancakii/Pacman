6e35c75f9772f9ef3e7dd39ecd10db646d03cd7d
EmirhanFaruk <emirhansagir74@gmail.com>
2023-12-06 12:18:52 +0100

    quick fix

--- src/main/java/gui/GameOver.java
+++ src/main/java/gui/GameOver.java
@@ -20,7 +20,7 @@ public class GameOver  {
                 App.menu.setHeight(500);
                 App.menu.setWidth(500);
                 //Il y a du CSS dans le setStyke, lien de l'image, est ce que ca se repete, la taille et la position
-                parent.setStyle("-fx-background-image: url('GOB.jpeg'); -fx-background-repeat: no-repeat;-fx-background-size: 500 500;-fx-background-position: left top;");
+                parent.setStyle("-fx-background-image: url('GOB.png'); -fx-background-repeat: no-repeat;-fx-background-size: 500 500;-fx-background-position: left top;");
                 // Ajouter un nom a la page
                 App.menu.setTitle("GameOver");
                 // Ne plus pouvoir aggrandir/retrecir la page
