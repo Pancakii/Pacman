01f7de00807360a91f8c342cef2061af0b410897
EmirhanFaruk <emirhansagir74@gmail.com>
2023-12-20 00:33:03 +0100

    Done with Ghost.java. Was already done with PacMan.java, will now do Ghost_pf.java.

--- src/main/java/model/Ghost.java
+++ src/main/java/model/Ghost.java
@@ -32,7 +32,7 @@ public enum Ghost implements Critter {
 
     /**
      * Gets eaten.
-     * @return
+     * @return eaten
      */
     public boolean isEaten() {
         return eaten;
@@ -40,71 +40,106 @@ public enum Ghost implements Critter {
 
     /**
      * Sets eaten.
-     * @param eaten
+     * @param eaten to set.
      */
     public void setEaten(boolean eaten) {this.eaten = eaten;}
 
     /**
      * Gets frightened.
-     * @return
+     * @return frightened
      */
     public boolean isFrightened() {return frightened;}
 
     /**
      * Sets frightened.
-     * @param frightened
+     * @param frightened to set.
      */
     public void setFrightened(boolean frightened) {this.frightened = frightened;}
 
     /**
      * Gets path.
-     * @return
+     * @return path
      */
     public ArrayList<RealCoordinates> getPath() {return path;}
 
     /**
      * Sets path.
-     * @param path
+     * @param path to set.
      */
     public void setPath(ArrayList<RealCoordinates> path) {this.path = path;}
 
     /**
      * Gets path_finding_timer_max.
-     * @return
+     * @return path_finding_timer_max
      */
     public double getPath_finding_timer_max() {return path_finding_timer_max;}
 
     /**
      * Gets path_finding_timer.
-     * @return
+     * @return path_finding_timer
      */
     public double getPath_finding_timer() {return path_finding_timer;}
 
     /**
      * Sets path_finding_timer.
-     * @param path_finding_timer
+     * @param path_finding_timer to set.
      */
     public void setPath_finding_timer(double path_finding_timer) {this.path_finding_timer = path_finding_timer;}
 
+    /**
+     * Gets chase_timer_max.
+     * @return chase_timer_max
+     */
     public double getChase_timer_max() {return chase_timer_max;}
 
+    /**
+     * Sets chase_timer.
+     * @param chase_timer to set.
+     */
     public void setChase_timer(double chase_timer) {this.chase_timer = chase_timer;}
+
+    /**
+     * Gets chase_timer.
+     * @return chase_timer
+     */
     public double getChase_timer() {return chase_timer;}
 
+    /**
+     * Gets scatter_timer_max
+     * @return scatter_timer_max
+     */
     public double getScatter_timer_max() {return scatter_timer_max;}
 
+    /**
+     * Gets scatter_timer.
+     * @return scatter_timer
+     */
     public double getScatter_timer() {return scatter_timer;}
+
+    /**
+     * Sets scatter_timer.
+     * @param scatter_timer to set.
+     */
     public void setScatter_timer(double scatter_timer) {this.scatter_timer = scatter_timer;}
 
+    /**
+     * Gets Clyde_startPosition.
+     * @return Clyde_startPosition
+     */
     public static IntCoordinates getClyde_startPosition() {return Clyde_startPosition;}
 
 
-
+    /**
+     * Gets pos.
+     * @return pos
+     */
     @Override
-    public RealCoordinates getPos() {
-        return pos;
-    }
+    public RealCoordinates getPos() {return pos;}
 
+    /**
+     * Sets pos. Also sets Clyde_startPosition if it hasn't been set yet.
+     * @param newPos to set.
+     */
     @Override
     public void setPos(RealCoordinates newPos) {
         pos = newPos;
@@ -115,21 +150,29 @@ public enum Ghost implements Critter {
         }
     }
 
+    /**
+     * Sets direction.
+     * @param direction to set.
+     */
     @Override
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
+    public void setDirection(Direction direction) {this.direction = direction;}
 
+    /**
+     * Gets direction.
+     * @return direction
+     */
     @Override
-    public Direction getDirection() {
-        return direction;
-    }
+    public Direction getDirection() {return direction;}
 
-    public void resetPath()
-    {
-        path = new ArrayList<>();
-    }
+    /**
+     * Sets path a new ArrayList.
+     */
+    public void resetPath() {path = new ArrayList<>();}
 
+    /**
+     * Gets speed depending on if near wormwall, frightened or eaten.
+     * @return double for speed
+     */
     @Override
     public double getSpeed() {
         double res = frightened ? 2.0 * nearWormWall() : 2.8 * nearWormWall();
@@ -140,6 +183,10 @@ public enum Ghost implements Critter {
         return res;
     }
 
+    /**
+     * Checks if the ghost is near the wormwall. Returns 1.0 or 0.5 to be multiplied by speed later.
+     * @return double for speed multiplier
+     */
     public double nearWormWall()
     {
         if(pos.round().y() == 9 && (pos.round().x() < 5 || pos.round().x() > 15))
@@ -149,16 +196,30 @@ public enum Ghost implements Critter {
         return 1.0;
     }
 
+    /**
+     * Calls Ghost_pf.scatterChaseTimer, which handles the timers of scatter/chase mode.
+     * @param d delta time
+     */
     public void scatterChaseTimer(long d)
     {
         Ghost_pf.scatterChaseTimer(this, d);
     }
 
+    /**
+     * Calls Ghost_pf.getPath, which sets a new path for the ghost.
+     * @param grid Cell table
+     * @param mazeConfig mazeConfig
+     * @param delta delta time
+     * @param base_coord base coordinates
+     */
     public void getPath(Cell[][] grid, MazeConfig mazeConfig, long delta, RealCoordinates base_coord)
     {
         Ghost_pf.getPath(this, grid, mazeConfig, delta, base_coord);
     }
 
+    /**
+     * Calls Ghost_pf.followPath, which makes the ghost follow the path.
+     */
     public void followPath()
     {
         Ghost_pf.followPath(this);
