52b5dfefb1785a654540bd7c396293e5e4e9f956
EmirhanFaruk <emirhansagir74@gmail.com>
2023-12-20 18:29:56 +0100

    Did documenting Debug.java.

--- src/main/java/misc/Debug.java
+++ src/main/java/misc/Debug.java
@@ -1,6 +1,10 @@
 package misc;
 
 public class Debug {
+    /**
+     * Prints the given message with ">> DEBUG >> " prefix.
+     * @param message the said message
+     */
     public static void out(final String message) {
         // comment this out if you do not want to see the debug messages
         System.out.println(">> DEBUG >> " + message);
